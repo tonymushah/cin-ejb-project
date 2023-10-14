bce58f9d50429300d3b5f71053d77703fd63f413
tonymushah <tonyramanantsalama@gmail.com>
2023-10-14 18:57:41 +0300

    Generated project

---
+++ presidence/src/main/java/mg/tonymushah/itu/presidence/PresidenceApplication.java
@@ -0,0 +1,13 @@
+package mg.tonymushah.itu.presidence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresidenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresidenceApplication.class, args);
+	}
+
+}
---
+++ presidence/src/test/java/mg/tonymushah/itu/presidence/PresidenceApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.tonymushah.itu.presidence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresidenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
